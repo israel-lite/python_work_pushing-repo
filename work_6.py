edba7e8065732c03fbd3aaec1f9c20944eeecb17
israeliiii <israelite1804@gmail.com>
2025-09-02 03:17:40 -0700

    add:Write a function reverse_string(text) that returns the reversed version of a string.

---
+++ work_6.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    sequence = [0, 1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence[:n]
+
+print(fibonacci(7))  # [0, 1, 1, 2, 3, 5, 8]
+
